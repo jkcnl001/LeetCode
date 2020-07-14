e5938e5141cbda85d3ee465416b783fb2a225f62
chuhouding <jkcnl001@163.com>
2020-07-14 11:20:45 +0800

    update

--- ts/Tool/Heap.ts
+++ ts/Tool/Heap.ts
@@ -1,3 +1,7 @@
+export enum HeapType {
+    min,
+    max
+}
 const Heap = {
     /**
      * @description: 父节点下标 从0开始
@@ -30,10 +34,59 @@ const Heap = {
      * @return: 
      */
     buildMinHeap: function (data: number[]) {
-        for (let i = data.length - 1; i > 0; --i) {
-            this.up(i, data, true);
+        for (let i = data.length - 1; i > data.length >> 1; --i) {
+            this.up(i, data, HeapType.min, true);
         }
         return data;
+    },
+
+    /**
+     * @description: 数组转为最大堆 
+     * @param {number} 
+     * @param {number[]} 
+     * @return: 
+     */
+    buildMaxHeap: function (data: number[]) {
+        for (let i = data.length - 1; i > data.length >> 1; --i) {
+            this.up(i, data, HeapType.max, true);
+        }
+        return data;
+    },
+
+    /**
+     * @description: 检测是否可以上浮
+     * @param {number} 
+     * @param {number} 
+     * @param {HeapType} 
+     * @return: 
+     */
+    checkUp(value: number, parent: number, type: HeapType = HeapType.min) {
+        switch (type) {
+            case HeapType.min: {
+                return parent > value;
+            }
+            case HeapType.max: {
+                return parent < value;
+            }
+        }
+    },
+
+    /**
+     * @description: 检测是否可以下层
+     * @param {number} 
+     * @param {number} 
+     * @param {HeapType} 
+     * @return: 
+     */
+    checkDown(value: number, child: number, type: HeapType = HeapType.min) {
+        switch (type) {
+            case HeapType.min: {
+                return child < value;
+            }
+            case HeapType.max: {
+                return child > value;
+            }
+        }
     },
 
     /**
@@ -42,18 +95,18 @@ const Heap = {
      * @param {number[]} 堆
      * @param {boolean}  是否是初始化
      */
-    up: function (i: number, data: number[], isInit: boolean = false) {
+    up: function (i: number, data: number[], type: HeapType = HeapType.min, isInit: boolean = false) {
         let parentIndex = this.getParentIndex(i);
         if (isInit) {
             while (data[parentIndex] !== undefined) {
-                if (data[parentIndex] > data[i]) {
+                if (this.checkUp(data[i], data[parentIndex], type)) {
                     [data[parentIndex], data[i]] = [data[i], data[parentIndex]];
                 }
                 i = parentIndex;
                 parentIndex = this.getParentIndex(i);
             }
         } else {
-            while (data[parentIndex] > data[i]) {
+            while (this.checkUp(data[i], data[parentIndex], type)) {
                 [data[parentIndex], data[i]] = [data[i], data[parentIndex]];
                 i = parentIndex;
                 parentIndex = this.getParentIndex(i);
@@ -66,18 +119,18 @@ const Heap = {
      * @param {i} 下标 
      * @param {number[]} 堆
      */
-    down: function (i: number, data: number[], isInit: boolean = false) {
+    down: function (i: number, data: number[], type: HeapType = HeapType.min, isInit: boolean = false) {
         let minIndex = this.getMinChildIndex(i, data);
         if (isInit) {
             while (minIndex !== -1) {
-                if (data[i] > data[minIndex]) {
+                if (this.checkDown(data[i], data[minIndex], type)) {
                     [data[i], data[minIndex]] = [data[minIndex], data[i]];
                 }
                 i = minIndex;
                 minIndex = this.getMinChildIndex(i, data);
             }
         } else {
-            while (minIndex !== -1 && data[i] > data[minIndex]) {
+            while (minIndex !== -1 && this.checkDown(data[i], data[minIndex], type)) {
                 [data[i], data[minIndex]] = [data[minIndex], data[i]];
                 i = minIndex;
                 minIndex = this.getMinChildIndex(i, data);
@@ -91,10 +144,21 @@ const Heap = {
      * @param {number[]}
      */
     getMinChildIndex: function (i: number, data: number[]) {
-        let leftIndex = this.getLeftIndex(i);
-        let rightIndex = this.getRightIndex(i);
+        const leftIndex = this.getLeftIndex(i);
+        const rightIndex = this.getRightIndex(i);
         return data[leftIndex] < data[rightIndex] ? leftIndex : rightIndex;
-    }
+    },
 
+    /**
+     * @description: 
+     * @param {number} 
+     * @param {number[]}
+     * @return: 
+     */
+    getMaxChildIndex: function (i: number, data: number[]) {
+        const leftIndex = this.getLeftIndex(i);
+        const rightIndex = this.getRightIndex(i);
+        return data[leftIndex] > data[rightIndex] ? leftIndex : rightIndex;
+    }
 }
 export default Heap;
